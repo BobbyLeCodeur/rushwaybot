299e98c82c883810473def0e9a44ec6eda40c512
BobbyLeCodeur <44397998+BobbyLeCodeur@users.noreply.github.com>
2018-10-24 11:38:19 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -19,7 +19,6 @@ bot.on("message", async message => {
     
     if (message.member.id != '396722578812829700') return message.channel.send(":x: • Vous n´avez pas la permission d´exécuter cette commande !");
    
-    else return message.channel.send(game);
       let game = args.slice.apply(1).join(' ');
       message.channel.send(game);
       bot.user.setActivity(game)
