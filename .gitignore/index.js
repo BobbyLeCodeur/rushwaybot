da77f7dcf5eb8659356aa1a154281aad15b124ef
BobbyLeCodeur <44397998+BobbyLeCodeur@users.noreply.github.com>
2018-10-31 16:16:50 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -18,7 +18,7 @@ bot.on("message", async message => {
   var args = message.content.substring(prefix.length).split(" ");
 
 bot.on("guildMemberAdd", member => {
-  const bvn = member.guild.channels.find(m => m.name === "👋🏻╿вιєηνєηυє")
+  const bvn = member.guild.channels.find(m => m.name === "bienvenue")
   if (!bvn) return;
   const embed = new Discord.RichEmbed()
   .setColor('#01B2FE')
@@ -31,7 +31,7 @@ bot.on("guildMemberAdd", member => {
 })
 
 bot.on("guildMemberRemove", member => {
-  const bvn = member.guild.channels.find(m => m.name === "👋🏻╿вιєηνєηυє")
+  const bvn = member.guild.channels.find(m => m.name === "bienvenue")
   if (!bvn) return;
   const embed = new Discord.RichEmbed()
   .setColor('#FE0101')
